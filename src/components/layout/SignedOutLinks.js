ef14a978326bad4a050f69b9d38e243daa51eedc
ihor <onyshchukihor11@gmail.com>
2020-06-12 14:11:50 +0300

    add Nav Links & styles

--- src/components/layout/SignedOutLinks.js
+++ src/components/layout/SignedOutLinks.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const SignedOutLinks = () => (<ul className="right">
-  <li><NavLink to='/'>Signup</NavLink></li>
-  <li><NavLink to='/'>Login</NavLink></li>
+  <li><NavLink to='/signup'>Signup</NavLink></li>
+  <li><NavLink to='/signin'>Login</NavLink></li>
 </ul>)
 
 
