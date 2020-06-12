0512184f5b2044f307437f63d2c49d0e23e80c74
ihor <onyshchukihor11@gmail.com>
2020-06-12 13:06:13 +0300

    add Create Project form

---
+++ src/components/projects/CreateProject.js
@@ -0,0 +1,41 @@
+import React, { PureComponent } from 'react'
+
+class CreateProject extends PureComponent {
+  state = {
+    title: '',
+    content: ''
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
+  handleInputChange = (e) => {
+    this.setState({
+      [e.target.id]: e.target.value
+    })
+
+  }
+  render() {
+    return (
+      <div className="container">
+        <form onSubmit={this.handleSubmit} className="white">
+          <h5 className="grey-text text-darken-3">Create Project</h5>
+          <div className="input-field">
+            <label htmlFor="title">Title</label>
+            <input type="text" id="title" onChange={this.handleInputChange} />
+          </div>
+          <div className="input-field">
+            <label htmlFor="content">Project Content</label>
+            <textarea id="content" className="materialize-textarea" onChange={this.handleInputChange}></textarea>
+          </div>
+          <div className="input-field">
+            <button className="btn pink lighten-1 z-depth-0">Create</button>
+          </div>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default CreateProject;
