c8130320ed5fe31a0a6d2852d04d766d68c91ba7
ihor <onyshchukihor11@gmail.com>
2020-06-12 12:36:18 +0300

    add Login & Signup components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
+import SignIn from './components/auth/SignIn';
+import SignUp from './components/auth/SignUp';
 import Navbar from './components/layout/Navbar';
 import Dashboard from './components/dashboard/Dashboard';
 import ProjectDetails from './components/projects/ProjectDetails';
@@ -13,6 +15,8 @@ function App() {
 				<Switch>
 					<Route path='/' component={Dashboard} exact />
 					<Route path='/project/:id' component={ProjectDetails} />
+					<Route path='/signin' component={SignIn} />
+					<Route path='/signup' component={SignUp} />
 				</Switch>
 			</div>
 		</BrowserRouter >
