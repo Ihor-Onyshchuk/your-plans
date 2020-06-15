7a97a7360a6a3fbf5bf58ff28de9207c9fe76588
ihor <onyshchukihor11@gmail.com>
2020-06-15 19:14:43 +0300

    add traking auth status

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -5,19 +5,23 @@ import { Link } from 'react-router-dom';
 import SignedInLinks from './SignedInLinks';
 import SignedOutLinks from './SignedOutLinks';
 
-const Navbar = ({ name = 'Your' }) => {
-  return <nav className="nav-wrapper grey darken-3">
-    <div className="container">
-      <Link to='/' className="brand-logo">{`${name}Plans`}</Link>
-      <SignedInLinks />
-      <SignedOutLinks />
-    </div>
-  </nav>
+const Navbar = ({ auth }) => {
+  const links = auth.uid ? <SignedInLinks /> : <SignedOutLinks />;
+  return (
+    <nav className="nav-wrapper grey darken-3">
+      <div className="container">
+        <Link to='/' className="brand-logo">YourPlans</Link>
+        {links}
+      </div>
+    </nav>
+  )
 }
 
 const mapStateToProps = (state) => {
   console.log(state);
-  return {}
+  return {
+    auth: state.firebase.auth
+  }
 }
 
 export default connect(mapStateToProps)(Navbar);
