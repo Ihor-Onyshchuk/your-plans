466615d019411e8006e0b43781188f754e29b93b
ihor <onyshchukihor11@gmail.com>
2020-06-26 17:45:52 +0300

    change tab size

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -8,46 +8,46 @@ import Notifications from './Notifications';
 import ProjectList from '../projects/ProjectList';
 
 class Dashboard extends PureComponent {
-  render() {
-    // console.log('props in dashboard', this.props);
-    const { projects, auth, notifications } = this.props;
-    if (!auth.uid) return <Redirect to='/signin' />;
+	render() {
+		// console.log('props in dashboard', this.props);
+		const { projects, auth, notifications } = this.props;
+		if (!auth.uid) return <Redirect to='/signin' />;
 
-    return (
-      <div className="dashboard container">
-        <div className="row">
-          <div className="col s12 m6">
-            <ProjectList projects={projects} />
-          </div>
-          <div className="col s12 m5 offset-m1">
-            <Notifications notifications={notifications} />
-          </div>
-        </div>
-      </div>
-    )
-  }
+		return (
+			<div className="dashboard container">
+				<div className="row">
+					<div className="col s12 m6">
+						<ProjectList projects={projects} />
+					</div>
+					<div className="col s12 m5 offset-m1">
+						<Notifications notifications={notifications} />
+					</div>
+				</div>
+			</div>
+		)
+	}
 }
 
 const mapStateToProps = (state) => {
-  console.log('dashboard firestore notifications', state.firestore.ordered.notifications);
-  return {
-    projects: state.firestore.ordered.projects,
-    auth: state.firebase.auth,
-    notifications: state.firestore.ordered.notifications
-  }
+	console.log('dashboard firestore notifications', state.firestore.ordered.notifications);
+	return {
+		projects: state.firestore.ordered.projects,
+		auth: state.firebase.auth,
+		notifications: state.firestore.ordered.notifications
+	}
 }
 
 export default compose(
-  connect(mapStateToProps),
-  firestoreConnect([
-    {
-      collection: 'projects',
-      orderBy: ['createdAt', 'desc']
-    },
-    {
-      limit: 3,
-      orderBy: ['time', 'desc'],
-      collection: 'notifications',
-    }
-  ])
+	connect(mapStateToProps),
+	firestoreConnect([
+		{
+			collection: 'projects',
+			orderBy: ['createdAt', 'desc']
+		},
+		{
+			limit: 3,
+			orderBy: ['time', 'desc'],
+			collection: 'notifications',
+		}
+	])
 )(Dashboard);
